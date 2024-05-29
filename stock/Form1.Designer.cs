84d0fd723c69705a1808fbcd3c764e5dbb2a9e19
farukerdem34 <farukomererdem2003@gmail.com>
2024-05-29 21:24:49 +0300

    Stock Database

---
+++ stock/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace stock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.tb_count = new System.Windows.Forms.TextBox();
+            this.tb_totalPrice = new System.Windows.Forms.TextBox();
+            this.tb_price = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(372, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(372, 163);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(372, 215);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 35);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(499, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(714, 425);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LightSalmon;
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.tb_id);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tb_price);
+            this.groupBox1.Controls.Add(this.tb_totalPrice);
+            this.groupBox1.Controls.Add(this.tb_count);
+            this.groupBox1.Controls.Add(this.tb_name);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(344, 425);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Stock";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(158, 76);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(180, 29);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Electronic",
+            "Furniture",
+            "Cleaning"});
+            this.comboBox1.Location = new System.Drawing.Point(158, 111);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 32);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Choose one...";
+            // 
+            // tb_name
+            // 
+            this.tb_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tb_name.Location = new System.Drawing.Point(158, 149);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(180, 29);
+            this.tb_name.TabIndex = 2;
+            // 
+            // tb_count
+            // 
+            this.tb_count.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tb_count.Location = new System.Drawing.Point(158, 184);
+            this.tb_count.Name = "tb_count";
+            this.tb_count.Size = new System.Drawing.Size(180, 29);
+            this.tb_count.TabIndex = 3;
+            // 
+            // tb_totalPrice
+            // 
+            this.tb_totalPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tb_totalPrice.Location = new System.Drawing.Point(158, 254);
+            this.tb_totalPrice.Name = "tb_totalPrice";
+            this.tb_totalPrice.ReadOnly = true;
+            this.tb_totalPrice.Size = new System.Drawing.Size(180, 29);
+            this.tb_totalPrice.TabIndex = 4;
+            // 
+            // tb_price
+            // 
+            this.tb_price.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tb_price.Location = new System.Drawing.Point(158, 219);
+            this.tb_price.Name = "tb_price";
+            this.tb_price.Size = new System.Drawing.Size(180, 29);
+            this.tb_price.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(6, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(6, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Category";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(6, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(6, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Count";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(6, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 24);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Price";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(6, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 24);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Total Price";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(6, 44);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 24);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "ID";
+            // 
+            // tb_id
+            // 
+            this.tb_id.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tb_id.Location = new System.Drawing.Point(158, 41);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(180, 29);
+            this.tb_id.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1225, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_price;
+        private System.Windows.Forms.TextBox tb_totalPrice;
+        private System.Windows.Forms.TextBox tb_count;
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tb_id;
+    }
+}
+
---
+++ stock/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection cnt = null;
+        SqlCommand cmd = null;
+        SqlDataAdapter adp = null;
+        DataSet ds = null;
+
+        public void Fill_Table()
+        {
+            cnt = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=stock;Integrated Security=True");
+            adp = new SqlDataAdapter("SELECT * FROM stock_table", cnt);
+            ds = new DataSet();
+            cnt.Open();
+            adp.Fill(ds, "stock_table");
+            dataGridView1.DataSource = ds.Tables["stock_table"];
+            cnt.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Fill_Table();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand();
+            cnt.Open();
+            cmd.Connection = cnt;
+            int total_price = calculate_total_price();
+
+            cmd.CommandText = "INSERT INTO stock_table(date,class,item,count,price,tprice) VALUES" +
+                "('" + dateTimePicker1.Value.ToString() + "','" + comboBox1.SelectedItem.ToString() + "','" + tb_name.Text + "','" + tb_count.Text + "','" + tb_price.Text + "','" + total_price.ToString() + "')";
+            cmd.ExecuteNonQuery();
+            cnt.Close();
+            Fill_Table();
+        }
+
+        private int calculate_total_price()
+        {
+            int price = Convert.ToInt32(tb_price.Text);
+            int total_price = Convert.ToInt32(tb_count.Text) * price;
+            return total_price;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand();
+            cnt.Open();
+            cmd.Connection = cnt;
+            cmd.CommandText = "DELETE FROM stock_table WHERE id='" + tb_id.Text.ToString() + "'";
+            cmd.ExecuteNonQuery();
+            cnt.Close();
+            Fill_Table();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int total_price = calculate_total_price();
+            cmd = new SqlCommand();
+            cnt.Open();
+            cmd.Connection = cnt;
+            cmd.CommandText = "UPDATE stock_table " +
+                "date='"+dateTimePicker1.Value.ToString()+"',class='"+comboBox1.SelectedItem.ToString()+"'," +
+                "item='"+tb_name.Text+"',count='"+tb_count.Text+"',price='"+tb_price.Text+"'," +
+                "tprice='"+total_price.ToString()+"' " +
+                "WHERE id='"+tb_id.Text+"'";
+            cmd.ExecuteNonQuery();
+            cnt.Close();
+            Fill_Table();
+        }
+    }
+}
